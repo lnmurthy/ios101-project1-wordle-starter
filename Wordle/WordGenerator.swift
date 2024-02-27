adf7fc42147d20bad0736d58c176c10ab338aaff
Leisha Murthy <60281490+lnmurthy@users.noreply.github.com>
2024-02-27 15:27:33 -0800

    Project1-Complete

--- Wordle/WordGenerator.swift
+++ Wordle/WordGenerator.swift
@@ -8,15 +8,22 @@
 import Foundation
 
 class WordGenerator {
-  // Exercise 8: Add more possible words and return a **random** string every time `generateRandomWord` is called
-  // IMPORTANT: MAKE SURE ALL THE LETTERS IN THE NEW WORDS YOU ADD ARE CAPITALIZED (e.g. "AUDIO" instead of "audio")
-  // Tip: Look at the Array API documentation to see what method returns a random element
-  // Checkpoint: After finishing this exercise, you should now have a different goal word each time you run the app! Try printing out the selected goal word to check. If it's not working, check that you have done this exercise correctly.
-  static let possibleWords = ["ABCDE"]
-  static func generateRandomWord() -> String? {
-    // START YOUR CODE HERE
-    return possibleWords.first
-    // END YOUR CODE HERE
-  }
+    // Exercise 8: Add more possible words and return a **random** string every time `generateRandomWord` is called
+    // IMPORTANT: MAKE SURE ALL THE LETTERS IN THE NEW WORDS YOU ADD ARE CAPITALIZED (e.g. "AUDIO" instead of "audio")
+    // Tip: Look at the Array API documentation to see what method returns a random element
+    // Checkpoint: After finishing this exercise, you should now have a different goal word each time you run the app! Try printing out the selected goal word to check. If it's not working, check that you have done this exercise correctly.
+    static let possibleWords = ["HANDS", "LEMON", "PEELS", "STAIR", "LEMON"]
+    
+    static func generateRandomWord() -> String? {
+        // START YOUR CODE HERE
+        // Use the randomElement() method to get a random word from the possibleWords array
+        if let randomWord = possibleWords.randomElement() {
+            print("goal word: \(randomWord)")
+            return randomWord
+        } else {
+            print("no goal word generated.")
+            return nil
+        }
+        // END YOUR CODE HERE
+    }
 }
-
